6b0d0c009481bbddf627b6864e03b82733d5e9cc
Borja Delgado Martín <borjadm.26@gmail.com>
2021-04-27 21:37:38 +0200

    Transactions: Add currency, ticker and transaction type models

--- vue-frontend/src/models/api/index.ts
+++ vue-frontend/src/models/api/index.ts
@@ -1,2 +1,5 @@
 export * from './user';
 export * from './transaction';
+export * from './currency';
+export * from './ticker';
+export * from './transactionType';
---
+++ vue-frontend/src/models/api/ticker.ts
@@ -0,0 +1,5 @@
+export interface Ticker {
+  id: number;
+  code: string;
+  name: string;
+}
