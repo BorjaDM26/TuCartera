9f5b403738da602c6ba1cb637c49badce1cb28dd
Borja Delgado Martín <borjadm.26@gmail.com>
2021-05-02 12:18:56 +0200

    Portfolios: Add ticker state and current value models in API

---
+++ asp-backend/TuCartera/TuCartera/Models/Tickers/DTO/TickerCurrentValueDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TuCartera.Models
+{
+    public class TickerCurrentValueDTO
+    {
+        public int Id { get; set; }
+        public float CurrentValue { get; set; }
+
+        public TickerCurrentValueDTO(int id, float value)
+        {
+            this.Id = id;
+            this.CurrentValue = value;
+        }
+    }
+}
