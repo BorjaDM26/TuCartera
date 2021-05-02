4c568bc77619d9c5b265b3b966668b512755fb8b
Borja Delgado Martín <borjadm.26@gmail.com>
2021-05-02 12:12:24 +0200

    Portfolios: Add portfolio models in API

---
+++ asp-backend/TuCartera/TuCartera/Models/Portfolios/DTO/PortfolioDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TuCartera.Models
+{
+    public class PortfolioDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsGlobal { get; set; }
+        public string Description { get; set; }
+        public List<int> TickerIds { get; set; }
+    }
+}
